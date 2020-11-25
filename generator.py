0edf83e5adf19ed8a1c5ad87dd7777aae7dd4178
Frederic Beaupre <49285197+fredbeaupre@users.noreply.github.com>
2020-11-24 22:08:31 -0500

    Generator summary

--- generator.py
+++ generator.py
@@ -1,18 +1,28 @@
 from keras.datasets import mnist
+from keras import models
+from keras import layers
 import matplotlib.pyplot as plt
 
 
-def main():
-    print("Hello world!")
+def generator_model(dims):
+    model = models.Sequential()
+    n_nodes = 128 * 7 * 7
+    model.add(layers.Dense(n_nodes, input_dim=dims))
+    model.add(layers.LeakyReLU(alpha=0.2))
+    model.add(layers.Reshape((7, 7, 128)))
+    # upsampling
+    model.add(layers.Conv2DTranspose(
+        128, (4, 4), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU(alpha=0.2))
+    # upsampling
+    model.add(layers.Conv2DTranspose(
+        128, (4, 4), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU(alpha=0.2))
+    model.add(layers.Conv2D(1, (7, 7), activation='sigmoid', padding='same'))
+    return model
 
 
 if __name__ == "__main__":
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    print(f'train data: {x_train.shape}, {y_train.shape}')
-    print(f'test data: {x_test.shape}, {y_test.shape}')
-
-    for i in range(100):
-        plt.subplot(10, 10, 1 + i)
-        plt.axis('off')
-        plt.imshow(x_train[i], cmap='gray_r')
-    plt.show()
+    dims = 500
+    generator = generator_model(dims)
+    generator.summary()
